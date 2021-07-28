6470a3ecee9b1a17fdbae05a169214c58d05cb78
Choi.SY <sollly0829@gmail.com>
2021-07-28 10:16:20 +0900

    Ex01 수정

--- src/day04_If/Ex01.java
+++ src/day04_If/Ex01.java
@@ -8,7 +8,7 @@ public class Ex01 {
  	 # if 조건식
  	  - if 조건식 : 참인 경우 종속문장 실행
  	  - else : if 조건이 거짓이면 무조건 종속문장 실행
- 	  - else는 if의 종속문장 다음에 무주건 들어와야 문법상 에러
+ 	  - else는 if의 종속문장 다음에 무주건 들어와야 문법상 에러가 없다
 	 */
 	
 	public static void main(String[] args) {
