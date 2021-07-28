72fd61c305c2c9971ee2d7f8e805e1dc839ea5c9
Choi.SY <sollly0829@gmail.com>
2021-07-28 10:14:33 +0900

    Ex01 if/else/else if

---
+++ src/day04_If/Ex01.java
@@ -0,0 +1,27 @@
+package day04_If;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	/*
+ 	 # if 조건식
+ 	  - if 조건식 : 참인 경우 종속문장 실행
+ 	  - else : if 조건이 거짓이면 무조건 종속문장 실행
+ 	  - else는 if의 종속문장 다음에 무주건 들어와야 문법상 에러
+	 */
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.print("수 입력 : ");
+		num=sc.nextInt();
+		if (num%3==0) {
+			System.out.println(num+"은 3의 배수");
+		} else {
+			System.out.println(num+"은 3의 배수 아님");
+		}
+		System.out.println("다음 문장들 실행");
+	}
+}
